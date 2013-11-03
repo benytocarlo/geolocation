ce9257215946b66701a5cb399630a76340d1924d
Rich Powell <powell.rich@gmail.com>
2013-11-02 20:24:58 -0700

    initial commit

---
+++ js/geoLocation.js
@@ -0,0 +1,67 @@
+var x=document.getElementById('geo');
+var lat;
+var longi;
+
+
+function getLocation()
+  {
+    if (Modernizr.geolocation)
+      {
+        navigator.geolocation.getCurrentPosition(showPosition, error_handler);
+      }
+    else{
+      x.innerHTML="Please enable location to use this application";
+    }
+  }
+
+function showPosition(position)
+  {
+     var mapOptions = {
+     zoom: 15,
+     center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+   }
+
+   var map = new google.maps.Map(document.getElementById("geo"), mapOptions);
+   var marker = new google.maps.Marker({
+      position: mapOptions.center,
+      map: map,
+      title: 'Hello World!'})
+   
+ }
+
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 8,
+//     center: new google.maps.LatLng(-28.643387, 153.612224),
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   }
+//   var map = new google.maps.Map(document.getElementById("geo"), mapOptions);
+// }
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDerj7Cy5bCKC_jsJawg5c91XQuENtB9oU&sensor=true&callback=getLocation";
+  document.body.appendChild(script);
+}
+function error_handler(err) {
+    if (err.code == 1) {
+       alert('error 1');
+    }
+
+    if (err.code == 2) {
+       alert('error 2');
+    }
+
+    if (err.code == 0) {
+       alert('error 3');
+    }
+}
+
+window.onload = function(){       
+//  getLocation();
+  loadScript();
+                           
+}
+
